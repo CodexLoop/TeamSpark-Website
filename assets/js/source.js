3cc7e462355ebd31aca5732fe4f78ded4b998f9e
Mnqobi Thusi <222017361@stu.ukzn.ac.za>
2025-05-26 18:34:27 +0200

    Scroll bug-fix for mobile

--- assets/js/source.js
+++ assets/js/source.js
@@ -1,67 +1,74 @@
 // Add header shadow
 const header = document.querySelector('.header');
 
-  function QueryShadow() {
-    if (window.scrollY > 40){
-      header.classList.add('shadow');
-    }
-    else {
-      header.classList.remove('shadow');
-    }
+function QueryShadow() {
+  if (window.scrollY > 40) {
+    header.classList.add('shadow');
   }
-  window.addEventListener('scroll', QueryShadow);
+  else {
+    header.classList.remove('shadow');
+  }
+}
+window.addEventListener('scroll', QueryShadow);
 
 // Trigger animations
-  const animatedElements = document.querySelectorAll('.animate-fade');
+const animatedElements = document.querySelectorAll('.animate-fade');
 
-  function revealOnScroll() {
-    const windowHeight = window.innerHeight;
+function revealOnScroll() {
+  const windowHeight = window.innerHeight;
 
-    animatedElements.forEach((el) => {
-      const position = el.getBoundingClientRect().top;
+  animatedElements.forEach((el) => {
+    const position = el.getBoundingClientRect().top;
 
-      if (position < windowHeight - 50) {
-        el.classList.add('visible');
-      }
-    });
-  }
-  window.addEventListener('scroll', revealOnScroll);
+    if (position < windowHeight - 50) {
+      el.classList.add('visible');
+    }
+  });
+}
+window.addEventListener('scroll', revealOnScroll);
 
 // Toggle menu btn
-  const menuBtn = document.querySelector('.toggle-menu');
-  const nav = document.querySelector('nav');
-  const icons = menuBtn.querySelectorAll('.menu-icon');
-  const navLinks = document.querySelectorAll("nav a");
+const menuBtn = document.querySelector('.toggle-menu');
+const nav = document.querySelector('nav');
+const icons = menuBtn.querySelectorAll('.menu-icon');
+const navLinks = document.querySelectorAll("nav a");
 
-  function ToggleMenu() {
-    nav.classList.toggle('close-menu');
-    icons.forEach(icon => {
-      icon.classList.toggle('close-icon');
-    });
-  }
-  menuBtn.addEventListener('click', ToggleMenu);
-  navLinks.forEach((link) => {
-    link.addEventListener('click', ToggleMenu);
+function ToggleMenu() {
+  nav.classList.toggle('close-menu');
+  icons.forEach(icon => {
+    icon.classList.toggle('close-icon');
   });
+}
+menuBtn.addEventListener('click', ToggleMenu);
+navLinks.forEach((link) => {
+  link.addEventListener('click', ToggleMenu);
+});
 
 // Add active link state
-  const sections = document.querySelectorAll("section[id]");
+const sections = document.querySelectorAll("section[id]");
 
-  window.addEventListener("scroll", () => {
-    let scrollY = window.pageYOffset;
+window.addEventListener("scroll", () => {
+  let scrollY = window.pageYOffset;
 
-    sections.forEach((section) => {
-      const sectionTop = section.offsetTop - 80;
-      const sectionHeight = section.offsetHeight;
-      const sectionId = section.getAttribute("id");
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop - 80;
+    const sectionHeight = section.offsetHeight;
+    const sectionId = section.getAttribute("id");
 
-      if (scrollY >= sectionTop && scrollY < sectionTop + sectionHeight) {
-        navLinks.forEach((link) => {
-          link.removeAttribute("aria-current");
-          if (link.getAttribute("href") === `#${sectionId}`) {
-            link.setAttribute("aria-current", "true");
-          }
-        })
-      }
-    })
-  })
+    if (scrollY >= sectionTop && scrollY < sectionTop + sectionHeight) {
+      navLinks.forEach((link) => {
+        link.removeAttribute("aria-current");
+        if (link.getAttribute("href") === `#${sectionId}`) {
+          link.setAttribute("aria-current", "true");
+        }
+      });
+    }
+  });
+});
+
+// Remove focus on elements
+document.addEventListener('mouseup', (e) => {
+  if (e.target.matches('button, a, svg, path')) {
+    e.target.blur();
+  }
+});
